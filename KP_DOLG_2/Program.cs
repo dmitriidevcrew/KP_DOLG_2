d7ff3cbe9f9008b541712e7f8fbe63ae832ddabd
dmitriidevcrew <dmitrii.pavlovich.dev@bk.ru>
2023-12-03 23:05:08 +0300

    Добавьте файлы проекта.

---
+++ KP_DOLG_2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите строку на русском языке:");
+string inputString = Console.ReadLine();
+
+string modifiedString = inputString.Replace('а', 'о');
+
+string filePath = "output.txt";
+
+// Запись в файл
+using (StreamWriter writer = new StreamWriter(filePath, true))
+{
+    writer.WriteLine($"Исходная строка: {inputString}");
+    writer.WriteLine($"Изменённая строка: {modifiedString}");
+}
+
+// Чтение из файла
+Console.WriteLine("\nПрошлые результаты:");
+using (StreamReader reader = new StreamReader(filePath))
+{
+    string line;
+    while ((line = reader.ReadLine()) != null)
+    {
+        Console.WriteLine(line);
+    }
+}
